ad6f1499a775aa11bf0e961947ca39d156c49434
Gubaz311 <bagusste25@gmail.com>
2023-11-21 07:14:30 +0700

    1

--- index.exs
+++ index.exs
@@ -1,9 +1,21 @@
-# Pendefinisian fungsi
-defmodule MyModule do
-  def my_function(x) do
-    IO.puts "Nilai yang diterima: #{x}"
+defmodule Elix.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :botMessage,
+     version: "0.1.0",
+     elixir: "~> 1.14.5",
+     elixirc_paths: elixirc_paths(Mix.env),
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps()]
+  end
+
+
+  def application do
+    [
+      mod: {Elix, []},
+      extra_applications: [:logger]
+    ]
   end
 end
-
-# Panggil fungsi
-MyModule.my_function(32)
